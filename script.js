6e5f84d9854b46a89e1556ad3da2b9e58ff3e917
Arnav <rajarnavpatna@gmail.com>
2024-06-16 23:23:53 +0530

    modified:   index.html 	deleted:    index2.html 	new file:   music.svg 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 console.log("js")
-async function main(){
-    let a = await fetch("file:///D:/htmlprojects/spotifyclone/songs/")
-    let response = a.text()
+async function getSongs(){
+    let a = await fetch("http://127.0.0.1:58710/songs/")
+    let response = await a.text()
     console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+
+    for (let index = 0; index < as.length; index++){
+        const element = as[index]
+        if (element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
+
+async function main(){
+    let songs = await getSongs()
+    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`
+    }
+
+    var audio = new Audio(songs[0])
+    // audio.play()
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    });
 }
 
 main()
