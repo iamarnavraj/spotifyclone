0742387b5bbf4e3e638b99f4875e75af080a750b
Arnav <rajarnavpatna@gmail.com>
2024-06-22 22:16:50 +0530

    new file:   favicon.ico 	modified:   index.html 	modified:   logo.svg 	new file:   pause.svg 	modified:   script.js 	renamed:    songs/Aaron Hibell - Destroyer Of worlds Oppenheimer (Trance Mix)(DjJpSwami.Com).mp3 -> songs/Aaron Hibell  - Copy.mp3 	new file:   songs/Aaron Hibell .mp3 	renamed:    songs/Middle Of The Night_128-(DJMaza).mp3 -> songs/Middle Of The Night - Copy.mp3 	new file:   songs/Middle Of The Night.mp3 	new file:   songs/ReelAudio-11760 - Copy.mp3 	new file:   songs/ReelAudio-46999 - Copy.mp3 	new file:   songs/ReelAudio-82275 - Copy.mp3 	renamed:    songs/Renegade_320(PaglaSongs).mp3 -> songs/Renegade_320 - Copy.mp3 	new file:   songs/Renegade_320.mp3 	deleted:    songs/Vengeance - I will die here - Instrumental.mp3 	new file:   songs/umbrella - Copy.mp3 	modified:   style.css

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-console.log("js")
+// console.log("js")
+let currentSong = new Audio()
+
+
+function convertSecondsToMinutes(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    // Format minutes and seconds to always be two digits
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    // Return the formatted time
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs(){
-    let a = await fetch("http://127.0.0.1:58710/songs/")
+    let a = await fetch("http://127.0.0.1:63817/songs/")
     let response = await a.text()
-    console.log(response)
+    // console.log(response)
     let div = document.createElement("div")
     div.innerHTML = response
     let as = div.getElementsByTagName("a")
@@ -17,21 +32,66 @@ async function getSongs(){
     return songs
 }
 
+const playMusic = (track) => {
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+    play.src = "pause.svg"
+
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = ("00:00 / 00:00")
+}
+
 async function main(){
     let songs = await getSongs()
-    console.log(songs)
+    // console.log(songs)
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`
+        songUL.innerHTML = songUL.innerHTML + 
+        `<li> 
+            <img class = "invert" src="music.svg">
+            <div class="info">
+                <div class = "flex">${song.replaceAll("%20", " ")} </div>
+                <div class = "flex">Arnav</div>
+            </div>
+            <div class="playnow">
+                <img src = "pla.svg">
+            </div>
+        
+        </li>`
     }
 
-    var audio = new Audio(songs[0])
+    // var audio = new Audio(songs[0])
     // audio.play()
 
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
-    });
+    // audio.addEventListener("loadeddata", () => {
+    //     console.log(audio.duration, audio.currentSrc, audio.currentTime)
+    // });
+
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })            
+    })
+
+    play.addEventListener("click", ()=>{
+        if (currentSong.paused){
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "pla.svg"
+        }
+    })
+
+    currentSong.addEventListener("timeupdate", ()=>{
+        console.log(currentSong.currentTime, currentSong.duration)
+        document.querySelector(".songtime").innerHTML = `${convertSecondsToMinutes(currentSong.currentTime)}/${convertSecondsToMinutes(currentSong.duration)}`
+    })
+    
 }
 
 main()
